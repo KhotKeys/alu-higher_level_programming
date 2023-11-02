ad72e048598e7ac7780340ada81dfda565711535
KhotKeys <gabrielkhotgarangpawuoi@gmail.com>
2023-11-02 09:13:00 -0700

    Gabriel

--- python-if_else_loops_functions/9-print_last_digit.py
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -1,14 +1,10 @@
 #!/usr/bin/python3
 def print_last_digit(number):
-    # Get the last digit by taking the absolute value of the number and then using modulo 10
-    last_digit = abs(number) % 10
-    print(last_digit, end='')
+    if number >= 0:
+        ld = number % 10
+    else:
+        ld = number % -10
+        ld *= -1
 
-# Testing the function
-if __name__ == "__main__":
-    print_last_digit = __import__('9-print_last_digit').print_last_digit
-
-    print_last_digit(98)
-    print_last_digit(0)
-    r = print_last_digit(-1024)
-    print(r)
+    print("{:d}".format(ld), end='')
+    return (ld)
