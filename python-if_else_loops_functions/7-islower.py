1b80323532f98fbcd397805d33f8a3005e2398ba
KhotKeys <gabrielkhotgarangpawuoi@gmail.com>
2023-11-02 06:19:36 -0700

    Gabriel Khot Keys Goodwin!

--- python-if_else_loops_functions/7-islower.py
+++ python-if_else_loops_functions/7-islower.py
@@ -1,7 +1,3 @@
 #!/usr/bin/python3
-def uppercase(str):
-    for i in str:
-        if ord("a") <= ord(i) <= ord("z"):
-            i = chr(ord(i) - 32)
-        print("{:s}".format(i), end="")
-    print()
+def islower(c):
+    return ord("a") <= ord(c) <= ord("z")
