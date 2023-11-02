b79322948b8065d27ed9100472643fdcb82daa68
KhotKeys <gabrielkhotgarangpawuoi@gmail.com>
2023-11-02 06:06:13 -0700

    Gabriel

--- python-if_else_loops_functions/7-islower.py
+++ python-if_else_loops_functions/7-islower.py
@@ -1,15 +1,3 @@
-#!/bin/
+#!/usr/bin/python3
 def islower(c):
-    # Check if the ASCII value of 'c' is in the range of lowercase letters (97-122)
-    if ord(c) >= 97 and ord(c) <= 122:
-        return True
-    else:
-        return False
-# Testing the function
-if __name__ == "__main__":
-    is_lower = __import__('7-islower').islower
-    print("a is {}".format("lower" if is_lower("a") else "upper"))
-    print("H is {}".format("lower" if is_lower("H") else "upper"))
-    print("A is {}".format("lower" if is_lower("A") else "upper"))
-    print("3 is {}".format("lower" if is_lower("3") else "upper"))
-    print("g is {}".format("lower" if is_lower("g") else "upper"))
+    return ord("a") <= ord(c) <= ord("z")
