a9e8258934bd8e3afbd228b7e4a2738b29de5d59
KhotKeys <gabrielkhotgarangpawuoi@gmail.com>
2023-11-05 02:06:40 -0800

    Gabriel Khot Garang

--- python-data_structures/0-print_list_integer.py
+++ python-data_structures/0-print_list_integer.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
-def print_list_integer(my_list=[]):
-    for num in my_list:
-        print("{:d}".format(num))
-if __name__ == "__main__":
-    my_list = [1, 2, 3, 4, 5]
-    print_list_integer(my_list)
+print_list_integer = __import__('0-print_list_integer').print_list_integer
+
+my_list = [1, 2, 3, 4, 5]
+print_list_integer(my_list)
