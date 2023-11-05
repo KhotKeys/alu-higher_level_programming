189ae42b6d217862ab8937c973f05a739911fce1
KhotKeys <gabrielkhotgarangpawuoi@gmail.com>
2023-11-05 02:13:24 -0800

    Python is the best language for the programmer

--- python-data_structures/0-print_list_integer.py
+++ python-data_structures/0-print_list_integer.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
-print_list_integer = __import__('0-print_list_integer').print_list_integer
-
+print_list_integer = __import__('0-print_list_integer').print_list_integer0
 my_list = [1, 2, 3, 4, 5]
 print_list_integer(my_list)
+formatted_code = """#!/usr/bin/python3
+print_list_integer = __import__('0-print_list_integer').print_list_integer
+my_list = [1, 2, 3, 4, 5]
+print_list_integer(my_list)"""
+print("Python script with str.format:")
+print(formatted_code.format(code=formatted_code))
