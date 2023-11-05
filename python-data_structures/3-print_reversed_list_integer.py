508dd7f3637f8f228ac6eb0b977520c42c823e11
KhotKeys <gabrielkhotgarangpawuoi@gmail.com>
2023-11-05 03:47:11 -0800

    Gabriel is cool!

--- python-data_structures/3-print_reversed_list_integer.py
+++ python-data_structures/3-print_reversed_list_integer.py
@@ -1,15 +1,9 @@
 #!/usr/bin/python3
-def replace_in_list(my_list, idx, element):
-    if idx < 0 or idx >= len(my_list):
-        return my_list
-    my_list[idx] = element
-    return my_list
-
-if __name__ == "__main__":
-    my_list = [1, 2, 3, 4, 5]
-    idx = 3
-    new_element = 9
-    new_list = replace_in_list(my_list, idx, new_element)
-
-    print(new_list)
-    print(my_list)
+def print_reversed_list_integer(my_list=[]):
+    """ prints all integers of a list, in reverse order."""
+    if not my_list:
+        pass
+    else:
+        my_list.reverse()
+        for i in range(len(my_list)):
+            print('{:d}'.format(my_list[i]))
