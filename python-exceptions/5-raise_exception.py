3113306f0e4afc2a2add539c1ca7be62954b406c
KhotKeys <gabrielkhotgarangpawuoi@gmail.com>
2023-11-08 04:54:43 -0800

    the term lostboy, comes with alot of confusion

---
+++ python-exceptions/5-raise_exception.py
@@ -0,0 +1,8 @@
+def raise_exception():
+    raise TypeError("This is a custom type exception")
+
+if __name__ == "__main__":
+    try:
+        raise_exception()
+    except TypeError as e:
+        print("Exception caught: ", e)
