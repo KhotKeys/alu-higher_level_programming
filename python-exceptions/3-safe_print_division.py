9b08a98f90e075240124ab8a87967f35822bfe80
KhotKeys <gabrielkhotgarangpawuoi@gmail.com>
2023-11-08 04:32:00 -0800

    Gabriel and Gabriella are like coke and tail :)

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 def safe_print_division(a, b):
+    try:
         result = a / b
     except ZeroDivisionError:
         result = None
@@ -8,6 +9,7 @@ def safe_print_division(a, b):
     return result
 
 if __name__ == "__main__":
+    # Example usage:
     a = 12
     b = 2
     result = safe_print_division(a, b)
