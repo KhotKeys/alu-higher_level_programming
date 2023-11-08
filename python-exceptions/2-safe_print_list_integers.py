1dfffe9a913df3306922df01e8713b0122c6589c
KhotKeys <gabrielkhotgarangpawuoi@gmail.com>
2023-11-08 04:25:04 -0800

    Gabriel and Gabriella are super programmers in python

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -10,10 +10,8 @@ def safe_print_list_integers(my_list=[], x=0):
     except (IndexError, ValueError, TypeError):
         pass
     return count
-
 if __name__ == "__main__":
     # Example usage:
     my_list = [1, "not_an_integer", 2, 3, "another_string"]
-
     nb_integers = safe_print_list_integers(my_list, 4)
     print("Number of integers printed: {:d}".format(nb_integers))
