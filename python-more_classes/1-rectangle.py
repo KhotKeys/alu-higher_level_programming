522697a134830310db770b3b9b34292d29dde732
KhotKeys <gabrielkhotgarangpawuoi@gmail.com>
2023-11-18 00:58:16 -0800

    Gabriel is an uanthetic individual

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -14,24 +14,23 @@ class Rectangle:
     @property
     def width(self):
         return self.__width
-    
+
     @width.setter
     def width(self, value):
         if not isinstance(value, int):
             raise TypeError("Width must be an integer")
-        if value < o:
+        if value < 0:
             raise ValueError("Width must be >= 0")
         self.__width = value
-    
+
     @property
     def height(self):
         return self.__height
 
     @height.setter
     def height(self, value):
-        if  not isinstance(value, int):
-            raise TypeError("height is an integer")
+        if not isinstance(value, int):
+            raise TypeError("Height must be an integer")
         if value < 0:
-            raise ValueError("height must be >= 0")
+            raise ValueError("Height must be >= 0")
         self.__height = value
-
