403f0c42073bfba55b034b9a2aab40485d8d5ef1
KhotKeys <gabrielkhotgarangpawuoi@gmail.com>
2023-11-19 13:12:13 -0800

    I am a software engineer!y

---
+++ python-classes/102-square.py
@@ -0,0 +1,84 @@
+#!/usr/bin/python3
+"""Defines a square"""
+
+'''
+file name = Python - Classes and Objects
+created = 19-11-2023
+Author = Gabriel Khot Garang Pawuoi
+size = undefine
+project = 102-square.py
+status = Not submitted
+'''
+class Square:
+     """
+    # Write a class square that defines a square:
+    # VARIABLE(" "):
+    # square(int): Simple square
+    # Return: (Success).
+    """
+
+    """ 'class square' defines the class is like a blueprint or template...
+    for creating objects...
+    while the 'pass' is used as a placeholder when you don't want to add...
+    any code in a block and this is just a sample of a 2D Polygon with a ...
+    ...4-Perpendicular Sides."""
+
+    def __init__(self, size=0):
+        """
+        initializes the square.
+        Args:
+        size: the size of the square
+        """
+        self.size = size
+
+    @property
+    def size(self):
+        """setter method for the size"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Setter method for size."""
+        if not isinstance(value, (int, float)):
+            raise TypeError("size must be a number")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """Calculates and returns the area of the square."""
+        return self.__size ** 2
+
+    def __eq__(self, other):
+        """Defines equality comparison."""
+        if isinstance(other, Square):
+            return self.area() == other.area()
+        return False
+
+    def __ne__(self, other):
+        """Defines inequality comparison."""
+        return not self.__eq__(other)
+
+    def __lt__(self, other):
+        """Defines less than comparison."""
+        if isinstance(other, Square):
+            return self.area() < other.area()
+        return False
+
+    def __le__(self, other):
+        """Defines less than or equal to comparison."""
+        if isinstance(other, Square):
+            return self.area() <= other.area()
+        return False
+
+    def __gt__(self, other):
+        """Defines greater than comparison."""
+        if isinstance(other, Square):
+            return self.area() > other.area()
+        return False
+
+    def __ge__(self, other):
+        """Defines greater than or equal to comparison."""
+        if isinstance(other, Square):
+            return self.area() >= other.area()
+        return False
