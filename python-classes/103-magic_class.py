a08e621b92ed92362cca68e801d41f47ba7e1d81
KhotKeys <gabrielkhotgarangpawuoi@gmail.com>
2023-11-19 12:22:15 -0800

    I am Gabriel Khot Garang

---
+++ python-classes/103-magic_class.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Defines a MagicClass."""
+
+import math
+
+class MagicClass:
+    """Represents a magic class."""
+
+    def __init__(self, radius=0):
+        """Initializes the MagicClass instance."""
+        self._MagicClass__radius = 0
+
+        if type(radius) is not int and type(radius) is not float:
+            raise TypeError('radius must be a number')
+
+        self._MagicClass__radius = radius
+
+    def area(self):
+        """Calculates the area of the MagicClass instance."""
+        return self._MagicClass__radius ** 2 * math.pi
+
+    def circumference(self):
+        """Calculates the circumference of the MagicClass instance."""
+        return 2 * math.pi * self._MagicClass__radius
