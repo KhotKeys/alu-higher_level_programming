db07dc2acf7b8325a072cec10938f6f6cf55d51c
KhotKeys <gabrielkhotgarangpawuoi@gmail.com>
2023-11-22 04:43:14 -0800

    Gabriel Khot Garang Pawuoi Khot is an authentic leader in the Tech side!

--- python-everything_is_object/100-magic_string.py
+++ python-everything_is_object/100-magic_string.py
@@ -1,4 +1,18 @@
 #!/usr/bin/python3
+
 def magic_string():
     magic_string.count = getattr(magic_string, 'count', 0) + 1
-    return "BestSchool" * magic_string.count
+    return "Holberton" * magic_string.count
+
+# Test case for main_0.py
+print(magic_string())
+
+# Test case for main_1.py
+for i in range(30):
+    print(magic_string())
+
+# Test case for main_2.py
+for i in range(13):
+    count, result = magic_string()
+    print(count)
+    print(result)
