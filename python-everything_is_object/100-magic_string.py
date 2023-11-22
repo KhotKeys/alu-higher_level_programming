cf80653b14cc5368cfc63155b78edd02448e2d50
KhotKeys <gabrielkhotgarangpawuoi@gmail.com>
2023-11-22 06:03:33 -0800

    Gabriel

--- python-everything_is_object/100-magic_string.py
+++ python-everything_is_object/100-magic_string.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-def magic_string(): magic_string.count = getattr(magic_string, 'count', 0) + 1; return "Holberton" * magic_string.count
-print(magic_string())
-print(magic_string()); print(magic_string())
-for i in range(30): print(magic_string())
+def magic_string(): magic_string.count = getattr(magic_string, 'count', 0) + 1;
+    return "Holberton" * magic_string.count
+print(magic_string(), magic_string(), magic_string(), end=" ");[print(magic_string()) for _ in range(30)]
