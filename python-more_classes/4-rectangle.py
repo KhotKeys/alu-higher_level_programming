ec08c2587a812019832df32fe87ccd5a86778c76
KhotKeys <gabrielkhotgarangpawuoi@gmail.com>
2023-11-22 14:03:25 -0800

    Gabriel Khot Keys

--- python-more_classes/4-rectangle.py
+++ python-more_classes/4-rectangle.py
@@ -76,6 +76,4 @@ class Rectangle:
 
     def __repr__(self):
         """Return the string representation of the Rectangle."""
-        rect = "Rectangle(" + str(self.__width)
-        rect += ", " + str(self.__height) + ")"
-        return rect
+        return f"Rectangle({self.width}, {self.height})"
