3e4918fc866b739ce33cc9728c6a3687f67b076b
KhotKeys <gabrielkhotgarangpawuoi@gmail.com>
2023-11-24 13:24:38 -0800

    Happiness

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -1,25 +1,30 @@
 #!/usr/bin/python3
-"""Public instance method: def area(self):"""
+'''Class Rectangle that inherits from BaseGeometry'''
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
-class BaseGeometry:
-    """Reprsent base geometry."""
+class Rectangle(BaseGeometry):
+    '''Rectangle class that inherits the BaseGeometry '''
 
-    def area(self):
-        """Not yet implemented."""
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """Validate a parameter as an integer.
+    def __init__(self, width, height):
+        '''Initialize a new Rectangle.
 
         Args:
-            name (str): The name of the parameter.
-            value (int): The parameter to validate.
-        Raises:
-            TypeError: If value is not an integer.
-            ValueError: If value is <= 0.
-        """
-        if type(value) != int:
-            raise TypeError("{} must be an integer".format(name))
-        if value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
+            width (int): The width of the new rectangle.
+            height (int): The height of the new rectangle.
+        '''
+        self.integer_validator('width', width)
+        self.integer_validator('height', height)
+        self.__width = width
+        self.__height = height
+
+    @property
+    def width(self):
+        """Get the width of the Rectangle."""
+        return self.__width
+
+    @property
+    def height(self):
+        """Get the height of the Rectangle."""
+        return self.__height
