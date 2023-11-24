81431dcaa4fc49d00d24c32b220337b09d68f434
KhotKeys <gabrielkhotgarangpawuoi@gmail.com>
2023-11-24 11:53:24 -0800

    Gabriel Khot

--- python-inheritance/11-square.py
+++ python-inheritance/11-square.py
@@ -1,24 +1,24 @@
 #!/usr/bin/python3
-"""Inherits BaseGeometry class"""
+'''Class Square that inherits from Rectangle'''
 
-
-BaseGeometry = __import__('7-base_geometry').BaseGeometry
 Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
-    """Inherits from Rectangle."""
+    '''Square class that inherits Rectangle'''
 
     def __init__(self, size):
-        """set the values"""
-        self.integer_validator("size", size)
+        '''A function that creates a square '''
+        self.integer_validator('size', size)
         super().__init__(size, size)
-        self.__size = size
-
-    def area(self):
-        """Area of Square"""
-        return self.__size ** 2
 
     def __str__(self):
-        '''Returns [Square] '''
-        return str("[Square] {}/{}".format(self.__size, self.__size))
+        '''Return the print() and str() representation of a Square '''
+        return "[Square] {}/{}".format(self.width, self.height)
+
+
+if __name__ == "__main__":
+    # Example Usage
+    s = Square(5)
+    print(s)
+    print(s.area())
