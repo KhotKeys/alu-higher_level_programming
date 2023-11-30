6d1b3f6e39341319e406566c4c9558814f3c0edf
root <gabrielkhotgarangpawuoi@gmail.com>
2023-11-30 20:05:15 +0000

    Gabriel

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -3,10 +3,14 @@
 
 if __name__ == '__main__':
     import urllib.request
+    from urllib.error import HTTPError
 
-    with urllib.request.urlopen('https://intranet.hbtn.io/status') as resp:
-        content = resp.read()
-        print("Body response:")
-        print("\t- type: {}".format(type(content)))
-        print("\t- content: {}".format(content))
-        print("\t- utf8 content: {}".format(content.decode('utf-8')))
+    try:
+        with urllib.request.urlopen('https://intranet.hbtn.io/status') as resp:
+            content = resp.read()
+            print("Body response:")
+            print("\t- type: {}".format(type(content)))
+            print("\t- content: {}".format(content))
+            print("\t- utf8 content: {}".format(content.decode('utf-8')))
+    except HTTPError as e:
+        print("HTTP Error: {}".format(e))
