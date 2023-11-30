02ee3e4233873397b70a8e7ce9459b29edbeb933
KhotKeys <gabrielkhotgarangpawuoi@gmail.com>
2023-11-30 11:38:37 +0000

    Gabriel

--- python-network_0/0-body_size.sh
+++ python-network_0/0-body_size.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
-
-# Check if URL is provided
-[ $# -eq 0 ] && { echo "Usage: $0 <URL>"; exit 1; }
-
-# Fetch Content-Length from URL and display the size
-url=$1; response_body=$(curl -sI $url | awk '/Content-Length/ {print $2}' | tr -d '\r'); [ -z "$response_body" ] && { echo "Error: Content-Length header not found in the response."; exit 1; }; echo "Size of the response body: ${response_body} bytes"
+# Receiving content and displaying content length information
+curl -sI "$1" | grep "Content-Length" | cut -d' ' -f 2
