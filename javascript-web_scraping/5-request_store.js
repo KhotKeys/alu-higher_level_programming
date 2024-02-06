5e3850d1a28590ff8b68f517a11c1bbf4078d50d
KhotKeys <gabrielkhotgarangpawuoi@gmail.com>
2024-02-06 21:35:22 +0000

    5-request_store.js

--- javascript-web_scraping/5-request_store.js
+++ javascript-web_scraping/5-request_store.js
@@ -5,10 +5,10 @@ const fs = require('fs');
 const path = process.argv[2];
 const file = process.argv[3];
 
-request(path, function (error, response, body){
-  if (error){
-    console.error('error');
-} else  {
+request(path, function (error, response, body) {
+  if (error) {
+    console.error(error);
+  } else {
     fs.writeFile(file, body, 'utf-8', function (error) {
       if (error) {
         console.error(error);
@@ -16,4 +16,3 @@ request(path, function (error, response, body){
     });
   }
 });
-
